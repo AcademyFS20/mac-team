8da287bc09ab8f6ea22242248c1bc419b34b2f1e
Marouane <m.marouane14@gmail.com>
2021-11-01 14:38:53 +0100

    QuizzJS

--- javascript/apps.js
+++ javascript/apps.js
@@ -2,26 +2,26 @@
 const quizPage = document.querySelector(".s-informations");
 const quizButtons = document.querySelector(".demarrer");
 
+let classActive = document.querySelector(".second")
+let classNon = document.getElementById("active")
+let progBar = document.getElementById("myProgress");
+
 
 quizButtons.addEventListener("click" , () => {
-let activated = document.querySelector(".activated");
-activated.style.backgroundColor = "#3278a9";
-activated.style.width = "2em";
-activated.style.heigth = "2em";
-activated.style.borderRadius = "2em";
-activated.style.border = "none"
+    classActive.classList.add("activated");
+    classNon.className += "second";
+    progBar.classList.remove("myProgress");
 
-
-
-let second = document.querySelector(".second");
-second.style.background = "#f0cf4b";
-second.style.border = "0.2em solid #91b6d0";
-second.style.boxSizing = "border-box";
-second.style.width = "2.1em";
-second.style.height = "2.1em";
-second.style.borderWidth = "12px"; 
-second.style.borderRadius = "30px"
 });
+
+
+
+
+
+
+
+
+
 
 
 
