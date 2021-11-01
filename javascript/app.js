f60e713b33b80af94e1c75531d983fe7491a5998
Nasserdine21 <nasserkrauser@gmail.com>
2021-11-01 19:41:14 +0100

    quiz_js_start

--- javascript/app.js
+++ javascript/app.js
@@ -1,216 +1,25 @@
-/* DOM JS */
-
 const quizPage = document.querySelector(".s-informations");
-
-const question = document.querySelector('#question')
-
-
-const quizBar = document.querySelector(".barre");
-
 const quizButtons = document.querySelector(".demarrer");
 
-const quizBtn = document.querySelector(".button");
 
-const quizProgress = "";
-
-
-
-
-/* QUESTIONS QUIZZ */
-
-let questions = [{
-        id: "1",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        choice3: "Ne sait pas",
-        answer: "1",
-    },
-    {
-        id: "2",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        choice3: "",
-        choice4: "Ne sait pas",
-        answer: "1",
-    }, {
-        id: "3",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    },
-    {
-        id: "4",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "5",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "6",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "7",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "8",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "9",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "10",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "11",
-        question: "",
-        choice1: "Bien",
-        choice2: "Assez Bien",
-        choice3: "Mal",
-        choice4: "Très mal",
-        answer: "1",
-    }, {
-        id: "12",
-        question: "",
-        choice1: "CHAMP LIBRE",
-        answer: "1",
-    }, {
-        id: "13",
-        question: "",
-        choice1: "< 50 ans",
-        choice2: "50-69 ans",
-        choice3: ">= 70 ans",
-        choice4: "< 15 ans",
-        answer: "1",
-    }, {
-        id: "14",
-        question: "",
-        choice1: "poids",
-        choice2: "taille",
-        answer: "1",
-    }, {
-        id: "15",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "16",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "17",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "18",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "19",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "20",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "21",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "22",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        answer: "1",
-    }, {
-        id: "23",
-        question: "",
-        choice1: "OUI",
-        choice2: "NON",
-        choice3: "Ne sait pas",
-        answer: "1",
-    },
-
-
-
-]
-
-console.log(questions);
-
-
-
-let currentQuestion = {}
-let acceptingAnswers = true
-let questionCounter = 0
-let availableQuestions = []
-
-const MAX_QUESTIONS = 23
-
-
-startQuiz = () => {
-    questionCounter = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
-}
-
-quizButtons.addEventListener("click", () => {
-    
-
-    let quitClass = quizPage.classList.remove(".informations");
-    let addClass = quizPage.classList.add(".informationsV2");
-
-    questionCounter++
-    quizPage.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-
-
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.questions
-
-    availableQuestions.splice(questionsIndex, 1)
-
-    acceptingAnswers = true
-
-
-    
-
-    
-
+quizButtons.addEventListener("click" , () => {
+let quizstart = document.querySelector("#infos");
+let point = document.querySelector("#les_infos");
+quizstart.classList.add("dispalyit");
+let quiz = document.querySelector(".start");
+quiz.classList.remove("start");
+point.classList.add("left");
+let btn = document.querySelector(".begin");
+btn.classList.add("start");
+let btns = document.querySelector(".questions");
+btns.classList.remove("questions");
+let informations = document.querySelector(".informations");
+informations.classList.add("start");
+let qsts = document.querySelector(".les_qsts");
+qsts.classList.remove("start");
+qsts.innerHTML=`<h1 id="text_qsts">شحال درجة الحرارة عندك ؟</h1>
+<div id="inpt">
+<label>درجة</label>
+<input type="number" id ="deg">
+<div>`;
 });
-
--- javascript/apps.js
+++ javascript/apps.js
@@ -4,23 +4,9 @@ const quizButtons = document.querySelector(".demarrer");
 
 
 quizButtons.addEventListener("click" , () => {
-let activated = document.querySelector(".activated");
-activated.style.backgroundColor = "#3278a9";
-activated.style.width = "2em";
-activated.style.heigth = "2em";
-activated.style.borderRadius = "2em";
-activated.style.border = "none"
 
 
 
-let second = document.querySelector(".second");
-second.style.background = "#f0cf4b";
-second.style.border = "0.2em solid #91b6d0";
-second.style.boxSizing = "border-box";
-second.style.width = "2.1em";
-second.style.height = "2.1em";
-second.style.borderWidth = "12px"; 
-second.style.borderRadius = "30px"
 });
 
 
