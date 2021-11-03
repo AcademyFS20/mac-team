6abeccb977bbb58a8e182c89e90455b7e6ad9e47
Nasserdine21 <nasserkrauser@gmail.com>
2021-11-03 17:17:33 +0100

    select_value_from_option

--- javascript/api.js
+++ javascript/api.js
@@ -16,6 +16,7 @@ console.error(error.message)
 // async geting promise data from function get data
 window.addEventListener("DOMContentLoaded", async () =>{
     let select = document.querySelector(".select");
+    let selectValue = document.querySelector(".select").value;
     const data = await getData(contries);
   const dataCountries = await getData(endpointWorld);
 
@@ -23,7 +24,7 @@ window.addEventListener("DOMContentLoaded", async () =>{
   
   let DataCountries = Countries.map((item) =>{
 
-       return {country:item.Country,totalConfirmed : item.TotalConfirmed,newConfirmed:item.NewConfirmed};
+       return {country:item.Country,totalConfirmed : item.TotalConfirmed,newConfirmed:item.NewConfirmed, totaldeath:item.TotalDeaths, newdeath:item.NewDeaths, recovery:item.TotalRecovered, newrecovery:item.NewRecovered, time:item.CountryCode};
   })
 
     let CountryM = data.filter((item) => {
@@ -56,8 +57,15 @@ window.addEventListener("DOMContentLoaded", async () =>{
       if(item.country === options[0].innerText) {
          
            const text = document.querySelector('.title');
-           text.innerHTML = item.totalConfirmed;
-           document.querySelector('.death').innerHTML = item.newConfirmed;
+           text.innerHTML = `<i class="fas fa-head-side-cough"></i>   Total Confirmed : ${item.totalConfirmed}`;
+           document.querySelector('.newconf').innerHTML = `<i class="fas fa-user-plus"></i>   New Confirmed : ${item.newConfirmed}`;
+           document.querySelector('.death').innerHTML = `<i class="fas fa-skull-crossbones"></i>  Total Deaths : ${item.totaldeath}`;
+           document.querySelector('.newdeath').innerHTML = `<i class="fas fa-user-plus"></i>   New Deaths : ${item.newdeath}`;
+           document.querySelector('.recovery').innerHTML = `<i class="fas fa-check"></i>  Total Recovered : ${item.recovery}`;
+           document.querySelector('.newrecovry').innerHTML = `<i class="fas fa-user-plus"></i> New Recovered : ${item.newrecovery}`
+           document.querySelector('.time').innerHTML = item.time;
       }
+    
     })
+    console.log(selectValue);
 });
