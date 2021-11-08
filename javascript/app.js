327908164dc477937e947f9d0893757255fd66e7
Nasserdine21 <nasserkrauser@gmail.com>
2021-11-08 16:33:01 +0100

    quiz_in_progress

--- javascript/app.js
+++ javascript/app.js
@@ -3,6 +3,17 @@ const quizButtons = document.querySelector(".demarrer");
 quizButtons.addEventListener("click" , () => {
 let bts = document.querySelector(".bts");
 let btp = document.querySelector(".btp");
+let age;
+let val;
+let fievre = 0;
+let FG = 0;
+let FGMin = 0;
+let FGMax = 0;
+let gorgeAndToux = 0;
+let santais;
+let fatigue;
+let FP = 0;
+age = Number(prompt("enter votre age"));
 let quizstart = document.querySelector("#infos");
 let currentIndex = 0;
 let point = document.querySelector("#les_infos");
@@ -28,7 +39,7 @@ function getJson(){
 
             bts.addEventListener("click", () =>{
                 if(currentIndex < qCount){
-                let theRightAnswer = jsoNQuestions[currentIndex].ranswer;
+            
                // console.log(theRightAnswer);
             }
                 currentIndex++;
@@ -36,8 +47,7 @@ function getJson(){
                 addQuestionData(jsoNQuestions[currentIndex], qCount);
             });
             btp.addEventListener("click", () =>{
-                let theRightAnswer = jsoNQuestions[currentIndex].ranswer;
-                console.log(theRightAnswer);
+                
                 currentIndex--;
                 qsts.innerHTML = "";
                 addQuestionData(jsoNQuestions[currentIndex] ,qCount);
@@ -65,6 +75,7 @@ function addQuestionData(obj, count){
         radioInput.id = String(`answer_${i}`);
         radioInput.dataset.answer = obj.choice[i - 1]
         radioInput.value= obj.choice[i - 1]
+        
         let label = document.createElement("label");
         label.htmlFor = `answer_${i}`;
         let labelText = document.createTextNode(obj.choice[i - 1]);
@@ -72,17 +83,67 @@ function addQuestionData(obj, count){
         mainDiv.appendChild(radioInput);
         mainDiv.appendChild(label);
         qsts.appendChild(mainDiv);
-       
-
-
-
-    }  
+           }  
 
       const el = document.querySelectorAll("input[type=radio]")
       el.forEach(element =>{
-          if(element.value === "oui"){
-          console.log("you check oui")
-          }
+          
+        element.addEventListener("change",(e) =>{
+            
+
+            
+            if(obj.id === "fever" && e.target.value === "oui"){
+                
+                val = Number(prompt("enter ur fever deg"));
+                if(val >= 37.8 && val <= 38.9){
+                    fievre++;
+                }
+                else if(val >= 39){
+                    fievre++;
+                    FGMin++;
+                    FG++;
+                }
+                else if(val <= 35.4){
+                    fievre++;
+                    FGMax++;
+                    FG++;
+                }
+                else if(val === ""){
+                    fievre++;
+                }
+                if(age < 50 && FG == 0 && FP == 0 && fievre > 0){
+                    console.log("Nous vous conseillons de rester à votre domicile et de contacter votre médecin en cas d'apparition de nouvaeux symptomes. Vous pourrez aussi utiliser à nouveau l'application pour réévaluer vos symptomes.");
+                }
+                else if(age >= 50 || age <= 69 && FG == 0 && FP ==0 && fievre > 0){
+                    console.log("Téléconsultation ou médecin généraliste ou visite à domicile.");
+                }
+                if(fievre > 0 && FP == 0 && FGMin >= 1){
+                    console.log("Téléconsultation ou médecin généraliste ou visite à domicile.");
+                }
+                else if(fievre > 0 && FP >= 1 && FG == 0){
+                    console.log("Téléconsultation ou médecin généraliste ou visite à domicile.");
+                }
+                else if(fievre > 0 && FP >= 1 && FGMin == 1){
+                    console.log("Téléconsultation ou médecin généraliste ou visite à domicile.");
+                }
+                else if(fievre > 0 && FP >= 1 && FGMin >= 2){
+                    console.log("appelez le 141");
+                }
+                else if(fievre > 0 && FP >= 0 && FGMax >= 1){
+                    console.log("appelez le 141");
+                }
+            }
+            if(obj.id === "toux"  &&  e.target.value === "oui" || obj.id==="gorge" &&  e.target.value === "oui"){
+
+                gorgeAndToux++;
+            
+            }
+            
+            if(gorgeAndToux === 2){
+                console.log("yes");
+            }
+            
+        })
       })
 }
 else{
